f2cd4d4544fbcf0d77d1a14334187c218c22ac06
Alexey Ospishchev <alexeyosp@gmail.com>
2021-04-06 23:24:11 +0300

    Fix name

--- YouTubeConverter/Program.cs
+++ YouTubeConverter/Program.cs
@@ -7,11 +7,10 @@ namespace YouTubeConverter
     {
         static void Main(string[] args)
         {
-            GetInfo();
-
+            GetInfoAsync();
         }
 
-        public static async void  GetInfo()
+        public static async void  GetInfoAsync()
         {
             var youtube = new YoutubeClient();
             var video = await  youtube.Videos.GetAsync("https://youtu.be/ErdJeQTpNbg");
